94fa4ec96e9ee53677100a0a392ee90e7dab3b62
KrisztianNagy <krisztian.nagy83@gmail.com>
2017-08-19 10:55:18 +0200

    added ace editor

--- src/app/game/genesis-sheets/edit-sheet/edit-sheet.component.ts
+++ src/app/game/genesis-sheets/edit-sheet/edit-sheet.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
 
+import { AceEditorComponent } from 'ng2-ace-editor';
 import { Editor } from 'primeng/primeng';
 
 @Component({
@@ -7,12 +8,45 @@ import { Editor } from 'primeng/primeng';
   templateUrl: './edit-sheet.component.html',
   styleUrls: ['./edit-sheet.component.scss']
 })
-export class EditSheetComponent implements OnInit {
+export class EditSheetComponent implements OnInit, AfterViewInit {
+  @ViewChild('editorhtml') editorhtml;
+  @ViewChild('editorcss') editorcss;
+
   private htmlText = '';
-  
+  private cssText = '';
+
+
   constructor() { }
 
   ngOnInit() {
   }
+
+  ngAfterViewInit() {
+    this.editorhtml.setTheme("eclipse");
+
+    this.editorhtml.getEditor().setOptions({
+        enableBasicAutocompletion: true
+    });
+
+    this.editorhtml.getEditor().commands.addCommand({
+        name: "showOtherCompletions",
+        bindKey: "Ctrl-.",
+        exec: function (editor) {
+        }
+    });
+
+    this.editorcss.setTheme("eclipse");
+    
+        this.editorcss.getEditor().setOptions({
+            enableBasicAutocompletion: true
+        });
+    
+        this.editorcss.getEditor().commands.addCommand({
+            name: "showOtherCompletions",
+            bindKey: "Ctrl-.",
+            exec: function (editor) {
+            }
+        });
+}
 
 }
